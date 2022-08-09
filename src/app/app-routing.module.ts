327c555169f1414916203ad242aee6cfc39939ee
tsiddheshwar <tsiddheshwar@gmail.com>
2022-08-09 19:26:12 +0530

    navbar improved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MgsFanpageComponent } from './landing-pages/mgs-fanpage/mgs-fanpage.component';
 import { SampleFormComponent } from './landing-pages/sample-form/sample-form.component';
+import { ConsolidatedResumeComponent } from './resume-components/consolidated-resume/consolidated-resume.component';
+import { LandingViewerComponent } from './landing-pages/landing-viewer/landing-viewer.component';
 
 const routes: Routes = [
+  { path: '', component: ConsolidatedResumeComponent },
   { path: 'mgsfanpage', component: MgsFanpageComponent },
   { path: 'sampleform', component: SampleFormComponent },
+  { path: 'skills', component: ConsolidatedResumeComponent },
+  { path: 'workdone', component: ConsolidatedResumeComponent },
+  { path: 'tools', component: ConsolidatedResumeComponent },
+  { path: 'landingpages', component: LandingViewerComponent },
+  { path: 'contact', component: ConsolidatedResumeComponent },
+  { path: '#', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-  constructor() { }
+  navbarItems: any = ['Skills', 'Work done', 'Tools', 'Landing Pages', 'Contact'];
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  clickNav(navbarItem: any) {
+    navbarItem == "Skills" ? this.router.navigate(['/', 'skills']) :
+      navbarItem == "Work done" ? this.router.navigate(['/', 'workdone']) :
+        navbarItem == "Tools" ? this.router.navigate(['/', 'tools']) :
+          navbarItem == "Landing Pages" ? this.router.navigate(['/', 'landingpages']) :
+            navbarItem == "Contact" ? this.router.navigate(['/', 'contact']) : '';
   }
 
 }
