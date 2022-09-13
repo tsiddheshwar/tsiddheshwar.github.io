445bffcd0d94346dc79845c56a5f3aba9ffca494
tsiddheshwar <tsiddheshwar@gmail.com>
2022-09-13 17:45:08 +0530

    modified technical skills

--- src/app/resume-components/technical-skills/technical-skills.component.ts
+++ src/app/resume-components/technical-skills/technical-skills.component.ts
@@ -8,12 +8,35 @@ import { ResumeResolverService } from '../resume-resolver.service';
   styleUrls: ['./technical-skills.component.scss']
 })
 export class TechnicalSkillsComponent implements OnInit {
-  technical_skills:any;
-  constructor(resumeResolverService:ResumeResolverService) {
+  technical_skills: any;
+  technologies: any = [];
+  technologies_types = ['programminglanguage', 'webtechnology', 'framework'];
+  tools: any = [];
+  tools_types = ["tool", "versioncontrolltool", "projectmanagementtool", "IDE"];
+  knowledge: any = [];
+  knowledge_types = ["knowledge"];
+  database: any = [];
+  database_types = ["database"];
+  main_types: any = [this.technologies, this.tools, this.database, this.knowledge];
+  constructor(resumeResolverService: ResumeResolverService) {
     this.technical_skills = JSON.parse(resumeResolverService.getResume()).technical_skills;
-   }
+  }
 
   ngOnInit(): void {
+    for (let i = 0; i < this.technical_skills.length; i++) {
+      if (this.technologies_types.includes(this.technical_skills[i].type)) {
+        this.technologies.push(this.technical_skills[i]);
+      }
+      else if (this.tools_types.includes(this.technical_skills[i].type)) {
+        this.tools.push(this.technical_skills[i]);
+      }
+      else if (this.knowledge_types.includes(this.technical_skills[i].type)) {
+        this.knowledge.push(this.technical_skills[i]);
+      }
+      else if (this.database_types.includes(this.technical_skills[i].type)) {
+        this.database.push(this.technical_skills[i]);
+      }
+    }
   }
 
 }
